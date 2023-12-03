b1c2dad0a10151d2741d5b1e0e92bbab25e3b3e5
kangjihyo <wlgy1003@naver.com>
2023-12-03 23:04:48 +0900

    studentGrade 구조체 실습과제

--- challenge/week13/studentGrade_struct.c
+++ challenge/week13/studentGrade_struct.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+//구조체 typedef를 이용하여 Student로 별칭 설정
 typedef struct {
 	char student_name[10];
 	int student_score;
 }Student;
 
+//구조체를 함수에서 사용하였음.
 int sumScores(Student* students, int student_number) { // for문을 이용하여 모든 값을 탐색하고 더함.
 	int sum = 0;
 	for (int i = 0; i < student_number; i++) {
@@ -47,7 +49,7 @@ void classifyStudents(Student* students, char targetGrade, int student_number) {
 	}
 }
 
-void printranks(Student* students, int student_number) {
+void printranks(Student* students, int student_number) { //첫 rank를 1로 설정하고 뒤의 점수가 더 높은 경우 rank를 증가시켜 등수 설정.
 	int rank;
 	for (int i = 0; i < student_number; i++) {
 		rank = 1;
@@ -66,12 +68,14 @@ int main() {
 	scanf_s("%d", &student_number);
 	//student_struct는 포인터 변수, 구조체 배열 갯수 동적으로 생성.
 	Student* students = (Student*)malloc(student_number * sizeof(Student));
+	//for문을 이용해서 구조체의 멤버에 값을 저장.
 	for (int i = 0; i < student_number; i++) {
 		printf("학생 %d의 이름을 입력하세요 : ", i+1);
 		scanf_s("%s", students[i].student_name, (int)sizeof(students[i].student_name));
 		printf("학생 %d의 점수를 입력하세요 : ", i + 1);
 		scanf_s("%d", &students[i].student_score);
 	}
+	//scanf로 인한 버퍼 제거
 	char ch = getchar();
 	char targetGrade;
 	printf("특정 점수 (A, B, C, D, F)를 입력하시오:");
